3264d527e4433bb26c31d2f44e2ceccd88975876
backfto <backfto@gmail.com>
2024-12-18 17:40:16 +0900

    third

--- C++/helloworld.cpp
+++ C++/helloworld.cpp
@@ -2,11 +2,7 @@
 using namespace std;
 
 int main() {
-	int n,m;
-	cin >> n >> m;
-	string arr[100000];
-	for(int i=0;i<n;i++){
-		string str;
-		cin >> arr[i];
-	}
+	cout << "hello world!" << endl;
+
+	return 0;
 }
