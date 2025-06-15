7cb74a183627280298f405c1e19f34710a1b5313
Jun <backfto@gmail.com>
2025-06-15 22:26:37 +0900

    250614.0

--- C++/2025-06/13/1822.cpp
+++ C++/2025-06/13/1822.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
+int n,m;
+int A[500000];
+int B[500000];
+
 int main() {
-    
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+    for(int i=0;i<n;i++) cin >> A[i];
+    for(int i=0;i<m;i++) cin >> B[i];
+    sort(A,A+n);
+    sort(B,B+m);
+    vector<int> ans;
+    for(int i=0;i<n;i++) {
+        if(!binary_search(B,B+m,A[i])) {
+            ans.push_back(A[i]);
+        }
+    }
+    cout << ans.size() << '\n';
+    for(int& a: ans) {
+        cout << a << ' ';
+    }
 }
