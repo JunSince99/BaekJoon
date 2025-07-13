06cea74f062d2db3ed59243f72a5121b395554b1
JunSince99 <93302860+JunSince99@users.noreply.github.com>
2025-07-13 21:02:57 +0900

    250713.0

---
+++ C++/2025-07/13/3711.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int stu[300];
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+    while(T--) {
+        int n;
+        cin >> n;
+        for(int i=0;i<n;i++) cin >> stu[i];
+        int m=1;
+        while (true) {
+            
+
+        }
+    }
+}
